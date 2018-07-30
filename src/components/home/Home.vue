fc61b8900645b03c7839154f8007f7f0b6564db8
wangqi <1191923306@qq.com>
2018-07-30 18:36:19 +0800

    project init

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div class="home">home</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
 }
 </script>
 
-<style>
+<style scoped lang="stylus">
+
 </style>
