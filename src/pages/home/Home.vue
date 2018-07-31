e87d01b7b9ed231050190da3b3f320a6e308e99c
wangqi <1191923306@qq.com>
2018-07-31 13:44:20 +0800

    add icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <Header></Header>
     <Swiper></Swiper>
+    <Icons></Icons>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import Swiper from './components/Swiper'
+import Icons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     Header,
-    Swiper
+    Swiper,
+    Icons
   }
 }
 </script>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-con" :src="item.imgUrl">
+          </div>
+          <p class="desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons >>> .swiper-container
+    height:0
+    padding-bottom: 50%
+  .icon
+    overflow: hidden
+    float: left
+    width: 25%
+    padding-bottom: 25%
+    position:relative
+    .icon-img
+      position:absolute
+      left:0
+      right:0
+      top:0
+      bottom:0.44rem
+      box-shadow: box-sizing
+      padding:0.1rem
+      .icon-img-con
+        height:100%
+        display: block
+        margin:0 auto
+    .desc
+      position:absolute
+      left:0
+      right: 0
+      bottom: 0
+      height:0.44rem
+      line-height:0.44rem
+      color:$darkTextColor
+      font-size:12px
+      text-align: center
+      ellipsis()
+</style>
